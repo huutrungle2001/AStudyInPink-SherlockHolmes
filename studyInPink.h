28978071fa5229af4a67d4856fbdedbc655f7166
huutrungle2001 <huutrungle2001@gmail.com>
2022-03-05 02:14:45 +0700

    Finish notebook1

--- studyInPink.h
+++ studyInPink.h
@@ -1,34 +1,83 @@
 /*
-* Ho Chi Minh City University of Technology
-* Faculty of Computer Science and Engineering
-* Initial code for Assignment 1
-* Programming Fundamentals Spring 2022
-* Author: Vu Van Tien
-* Date: 10.02.2022
-*/
+ * Ho Chi Minh City University of Technology
+ * Faculty of Computer Science and Engineering
+ * Initial code for Assignment 1
+ * Programming Fundamentals Spring 2022
+ * Author: Vu Van Tien
+ * Date: 10.02.2022
+ */
 
-//The library here is concretely set, students are not allowed to include any other libraries.
+// The library here is concretely set, students are not allowed to include any other libraries.
 #ifndef studyInPink_h
 #define studyInPink_h
 
-#include <iostream>
-#include <iomanip>
 #include <cmath>
-#include <string>
 #include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
 ////////////////////////////////////////////////////////////////////////
-///STUDENT'S ANSWER BEGINS HERE
-///Complete the following functions
-///DO NOT modify any parameters in the functions.
+/// STUDENT'S ANSWER BEGINS HERE
+/// Complete the following functions
+/// DO NOT modify any parameters in the functions.
 ////////////////////////////////////////////////////////////////////////
 
+bool isInt(string s) {
+    for (char c : s) {
+        if (!isdigit(c)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 string notebook1(string ntb1) {
-    // Complete this function to gain point
-    return "000000000";
+    // File stream
+    ifstream read(ntb1);
+
+    // Read the first line
+    string line;
+    getline(read, line);
+
+    // Get string n1
+    string temp(line, 11, 14);
+
+    // Check convertable
+    if (!isInt(temp)) {
+        return "0000000000";
+    }
+
+    // Convert to int
+    int n1 = stoi(temp);
+
+    // Array to count occurences
+    int count[10] = {};
+
+    // Read the second line
+    getline(read, line);
+
+    // Put the second line to string stream
+    stringstream ss(line);
+    int c;
+
+    // Read and count occurences of number in the second line
+    for (int i = 0; i < n1; i++) {
+        ss >> c;
+        count[c]++;
+    }
+
+    // Convert array to string
+    string occurrences = "";
+    for (int i : count) {
+        occurrences += to_string(i % 10);
+    }
+
+    read.close();
+    return occurrences;
 }
 
 string notebook2(string ntb2) {
@@ -50,9 +99,8 @@ bool chaseTaxi(
     int arr[100][100],
     string points,
     string moves,
-    string & outTimes,
-    string & outCatchUps
-) {
+    string& outTimes,
+    string& outCatchUps) {
     // Complete this function to gain point
     return false;
 }
@@ -63,6 +111,6 @@ string enterLaptop(string tag, string message) {
 }
 
 ////////////////////////////////////////////////
-///END OF STUDENT'S ANSWER
+/// END OF STUDENT'S ANSWER
 ////////////////////////////////////////////////
 #endif /* studyInPink_h */
