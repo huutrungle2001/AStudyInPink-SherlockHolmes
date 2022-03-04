4c30800497611f47a60f726bb70fc85f2a330a28
huutrungle2001 <huutrungle2001@gmail.com>
2022-03-05 01:18:53 +0700

    First commit

---
+++ studyInPink.h
@@ -0,0 +1,68 @@
+/*
+* Ho Chi Minh City University of Technology
+* Faculty of Computer Science and Engineering
+* Initial code for Assignment 1
+* Programming Fundamentals Spring 2022
+* Author: Vu Van Tien
+* Date: 10.02.2022
+*/
+
+//The library here is concretely set, students are not allowed to include any other libraries.
+#ifndef studyInPink_h
+#define studyInPink_h
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////
+///STUDENT'S ANSWER BEGINS HERE
+///Complete the following functions
+///DO NOT modify any parameters in the functions.
+////////////////////////////////////////////////////////////////////////
+
+string notebook1(string ntb1) {
+    // Complete this function to gain point
+    return "000000000";
+}
+
+string notebook2(string ntb2) {
+    // Complete this function to gain point
+    return "000000000";
+}
+
+string notebook3(string ntb3) {
+    // Complete this function to gain point
+    return "000000000";
+}
+
+string generateListPasswords(string pwd1, string pwd2, string pwd3) {
+    // Complete this function to gain point
+    return "000000000";
+}
+
+bool chaseTaxi(
+    int arr[100][100],
+    string points,
+    string moves,
+    string & outTimes,
+    string & outCatchUps
+) {
+    // Complete this function to gain point
+    return false;
+}
+
+string enterLaptop(string tag, string message) {
+    // Complete this function to gain point
+    return "";
+}
+
+////////////////////////////////////////////////
+///END OF STUDENT'S ANSWER
+////////////////////////////////////////////////
+#endif /* studyInPink_h */
