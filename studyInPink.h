2d7c1d9afb3b106e88eac3226d09b1424315e29e
huutrungle2001 <huutrungle2001@gmail.com>
2022-03-05 03:19:19 +0700

    Finish notebook2 30' ago

--- studyInPink.h
+++ studyInPink.h
@@ -1,13 +1,3 @@
-/*
- * Ho Chi Minh City University of Technology
- * Faculty of Computer Science and Engineering
- * Initial code for Assignment 1
- * Programming Fundamentals Spring 2022
- * Author: Vu Van Tien
- * Date: 10.02.2022
- */
-
-// The library here is concretely set, students are not allowed to include any other libraries.
 #ifndef studyInPink_h
 #define studyInPink_h
 
@@ -20,12 +10,7 @@
 
 using namespace std;
 
-////////////////////////////////////////////////////////////////////////
-/// STUDENT'S ANSWER BEGINS HERE
-/// Complete the following functions
-/// DO NOT modify any parameters in the functions.
-////////////////////////////////////////////////////////////////////////
-
+// Check if a string can be converted to int
 bool isInt(string s) {
     for (char c : s) {
         if (!isdigit(c)) {
@@ -47,7 +32,7 @@ string notebook1(string ntb1) {
     string temp(line, 11, 14);
 
     // Check convertable
-    if (!isInt(temp)) {
+    if (!isInt(temp) || stoi(temp) <= 0) {
         return "0000000000";
     }
 
@@ -80,9 +65,63 @@ string notebook1(string ntb1) {
     return occurrences;
 }
 
+// Encrypt cntP
+string encrypt(int cntP) {
+    if (cntP < 10000) {
+        cntP *= cntP;
+    }
+
+    string cipher = to_string(cntP);
+
+    while (cipher.size() < 10) {
+        cipher += '9';
+    }
+
+    return cipher;
+}
+
+// Count occurence of one string in another string
+int countOccurence(string s, string delim) {
+    int start = 0;
+    int end = s.find(delim);
+    int count = 0;
+    while (end != -1) {
+        count++;
+        start = end + delim.length();
+        end = s.find(delim, start);
+    }
+
+    return count;
+}
+
+// Count occurence of "pink" and "Pink" in a string
+int countPink(string s) {
+    return countOccurence(s, "pink") + countOccurence(s, "Pink");
+}
+
 string notebook2(string ntb2) {
-    // Complete this function to gain point
-    return "000000000";
+    // File stream
+    ifstream read(ntb2);
+
+    // Read the first line
+    string line;
+    getline(read, line);
+    int n2 = stoi(line);
+
+    // Verify n2
+    if (!isInt(line) || line.size() < 5 || n2 < 5 || n2 > 100) {
+        return "1111111111";
+    }
+
+    // Count pink
+    int cntP = 0;
+    for (int i = 0; i < n2; i++) {
+        getline(read, line);
+        cntP += countPink(line);
+    }
+
+    read.close();
+    return encrypt(cntP);
 }
 
 string notebook3(string ntb3) {
