7737a47cc95723917058dd3866eaa021f1ca878a
huutrungle2001 <huutrungle2001@gmail.com>
2022-03-05 05:01:42 +0700

    Finish note book 3

--- studyInPink.h
+++ studyInPink.h
@@ -124,9 +124,203 @@ string notebook2(string ntb2) {
     return encrypt(cntP);
 }
 
+#define N 10
+
+// Split a string of numbers into an array of numbers
+int* split(string& str, string delimiter) {
+    // Allocate memory for the array
+    int* row = new int[N];
+
+    int start = 0;
+    int end = str.find(delimiter);
+    int i = 0;
+
+    // Parse number one by one
+    while (end != -1) {
+        row[i] = abs(stoi(str.substr(start, end - start)));
+        start = end + delimiter.size();
+        end = str.find(delimiter, start);
+        i++;
+    }
+    row[i] = abs(stoi(str.substr(start, end - start)));
+
+    return row;
+}
+
+// Display a matrix
+void showMatrix(int** matrix) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+// Read matrix from file
+int** getMatrix(string ntb3) {
+    ifstream read(ntb3);
+
+    // Allocate memory for the matrix
+    int** matrix = new int*[N];
+    string line;
+    int i = 0;
+    // Read a string of numbers and turn it to an array of numbers
+    while (getline(read, line)) {
+        matrix[i] = split(line, "|");
+        i++;
+    }
+
+    read.close();
+    return matrix;
+}
+
+// Check if a number is prime number
+bool isPrime(int n) {
+    if (n < 2) {
+        return false;
+    }
+
+    for (int i = 2; i < sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Get the smallest prime number bigger than n
+int nextPrime(int n) {
+    if (n < 2) {
+        return 2;
+    }
+
+    while (!isPrime(n)) {
+        n++;
+    }
+
+    return n;
+}
+
+// Turn all number above the main diagonal into prime numbers
+void primeAbove(int** matrix) {
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            matrix[i][j] = nextPrime(matrix[i][j]);
+        }
+    }
+}
+
+// Fibonacci array
+int Fib[100] = {0};
+
+// Get the nth Fibonacci number
+int getFib(int n) {
+    if (n == 0) {
+        return 0;
+    }
+
+    if (n == 1 || n == 2) {
+        return (Fib[n] = 1);
+    }
+
+    if (Fib[n]) {
+        return Fib[n];
+    }
+
+    // If n is odd then k = (n + 1)/2
+    // F(n) = F(k)*F(k) + F(k-1)*F(k-1)
+    if (n & 1) {
+        int k = (n + 1) / 2;
+        Fib[n] = getFib(k) * getFib(k) + getFib(k - 1) * getFib(k - 1);
+    }
+
+    // If n is even then k = n/2:
+    // F(n) = (2*F(k-1) + F(k))*F(k)
+    else {
+        int k = n / 2;
+        Fib[n] = (2 * getFib(k - 1) + getFib(k)) * getFib(k);
+    }
+
+    return Fib[n];
+}
+
+// Get the smallest Fibonacci number biggther than n
+int nextFib(int n) {
+    int i = 0;
+    while (getFib(i) < n) {
+        i++;
+    }
+    return getFib(i);
+}
+
+// Turn all number below the main diagonal into Fibonacci number
+void FiboBelow(int** matrix) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            matrix[i][j] = nextFib(matrix[i][j]);
+        }
+    }
+}
+
+// Sort an array from index start to index end
+void sortRange(int* arr, int start, int end) {
+    for (int i = start; i <= end; i++) {
+        for (int j = i + 1; j <= end; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+// Sort three las columns of the matrix
+void sort3LastMatrixCols(int** matrix) {
+    for (int i = 0; i < N; i++) {
+        sortRange(matrix[i], 7, 9);
+    }
+}
+
+// Get the index of the largest element on a row
+int maxOnRow(int* row) {
+    int maxIndex = 0;
+
+    for (int i = 1; i < N; i++) {
+        if (row[i] >= row[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
+}
+
+// Get the code from the matrix
+string getCode(int** matrix) {
+    string cipher = "";
+
+    for (int i = 0; i < N; i++) {
+        cipher += to_string(maxOnRow(matrix[i]));
+    }
+
+    return cipher;
+}
+
 string notebook3(string ntb3) {
-    // Complete this function to gain point
-    return "000000000";
+    // Read matrix from file
+    int** matrix = getMatrix(ntb3);
+    // Turn all number above the main diagonal into prime numbers
+    primeAbove(matrix);
+    // Turn all number below the main diagonal into Fibonacci number
+    FiboBelow(matrix);
+    // Sort three las columns of the matrix
+    sort3LastMatrixCols(matrix);
+
+    // Get the code from the matrix
+    return getCode(matrix);
 }
 
 string generateListPasswords(string pwd1, string pwd2, string pwd3) {
